b67cc98f6924b07589101a04256992b8976dabdb
jernicozz <cendleader@gmail.com>
2020-03-18 14:28:18 +0300

    Initial Commit

---
+++ dbexample/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dbexample
+//
+//  Created by Дмитрий Маслюков on 18.03.2020.
+//  Copyright © 2020 Дмитрий Маслюков. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
